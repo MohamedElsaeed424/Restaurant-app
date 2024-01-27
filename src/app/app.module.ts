a5e1dc1b7b833acc4266104ac21aa0fe6f9dd9bc
MohamedElsaeed424 <mohamed.smenshawy@gmail.com>
2024-01-27 18:28:06 +0200

    components added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { ShoppingListComponent } from './header/shopping-list/shopping-list.component';
+import { RecipeListComponent } from './header/recipe-list/recipe-list.component';
+import { ShoppingListEditComponent } from './header/shopping-list/shopping-list-edit/shopping-list-edit.component';
+import { RecipeItemComponent } from './header/recipe-list/recipe-item/recipe-item.component';
+import { RecipeDetailsComponent } from './header/recipe-list/recipe-details/recipe-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ShoppingListComponent,
+    RecipeListComponent,
+    ShoppingListEditComponent,
+    RecipeItemComponent ,
+    RecipeDetailsComponent
   ],
   imports: [
     BrowserModule
