9ea0e92ca22222efdda725b631a1bc0eec9e5f35
MohamedElsaeed424 <mohamed.smenshawy@gmail.com>
2024-01-27 21:51:17 +0200

    dev

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,5 +7,12 @@ import { Component } from '@angular/core';
   styleUrl: './recipe-list.component.css'
 })
 export class RecipeListComponent {
+
+  recipes : Recipe [] = [
+    new Recipe('Mahshy' ,
+               'This is Test recipe' ,
+               'https://th.bing.com/th/id/R.80061b348256fa1aea364c0aa6a9e288?rik=biY8rhkLajBO3Q&pid=ImgRaw&r=0' )
+  ] ;
+
 
 }
