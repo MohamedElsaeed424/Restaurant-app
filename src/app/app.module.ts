40e99f42649715a4a2a6972a81fc9d906a00621b
MohamedElsaeed424 <mohamed.smenshawy@gmail.com>
2024-01-27 19:10:07 +0200

    dsa

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-ed
   declarations: [
     AppComponent,
     HeaderComponent,
+    RecipesComponent ,
     RecipeListComponent,
     RecipeDetailComponent,
     RecipeItemComponent,
