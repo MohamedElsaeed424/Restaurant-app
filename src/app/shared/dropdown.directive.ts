d0d3b2ab4e94c11f8f044c30d59c8e4bd84c1d3b
MohamedElsaeed424 <mohamed.smenshawy@gmail.com>
2024-02-04 12:04:40 +0200

    d]]dropdown added

--- src/app/shared/dropdown.directive.ts
+++ src/app/shared/dropdown.directive.ts
@@ -4,15 +4,26 @@ import { Directive, ElementRef, HostBinding, HostListener, Renderer2 } from "@an
     selector : '[appDropdown]'
 })
 
-export class DropdownDirective {
-    @HostBinding('class.open') isOpen : boolean = false ; 
+// export class DropdownDirective {
+//     @HostBinding('class.open') isOpen : boolean = false ; 
     
-    // constructor(private elementREF : ElementRef , private renderer : Renderer2){}
-    // @HostListener('click') openDropDown (eventData :  Event){
-    //     this.renderer.addClass(this.elementREF , 'open')
-    // }
+//     // constructor(private elementREF : ElementRef , private renderer : Renderer2){}
+//     // @HostListener('click') openDropDown (eventData :  Event){
+//     //     this.renderer.addClass(this.elementREF , 'open')
+//     // }
 
-    @HostListener('click')openDropDown(){
-        this.isOpen = !this.isOpen
+//     @HostListener('click')openDropDown(){
+//         this.isOpen = !this.isOpen
+//     }
+// }
+
+// To make the drop down closed when written on any place on the page 
+export class DropdownDirective {
+    constructor(private elRef: ElementRef) {}
+    
+    @HostBinding('class.open') isOpen = false;
+    @HostListener('document:click', ['$event']) openDropDown(event: Event) {
+      this.isOpen = this.elRef.nativeElement.contains(event.target) ? !this.isOpen : false;
     }
-}
+    
+  }
